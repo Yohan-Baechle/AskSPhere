9cbff74d0a392cb91daf2b9aac954c53f031530f
Yohan-Baechle <baechle.yohan.gmail.com>
2023-11-12 22:26:45 +0100

    Visuels form ok + bdd ok

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Question;
+use App\Form\CommentType;
 use App\Form\QuestionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,15 +16,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class QuestionController extends AbstractController
 {
   #[Route('/ask', name: 'form')]
-  public function index(Request $request): Response
+  public function index(Request $request, EntityManagerInterface $em): Response
   {
 
-    $formQuestion = $this->createForm(QuestionType::class);
+    $question = new Question();
+    $formQuestion = $this->createForm(QuestionType::class, $question);
 
     $formQuestion->handleRequest($request);
 
     if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
-      dd($formQuestion->getData());
+      $question->setNbrOfResponse(0);
+      $question->setRating(0);
+      $question->setCreatedAt(new \DateTimeImmutable());
+      $em->persist($question);
+      $em->flush();
+      $this->addFlash('success', 'Votre question a été ajoutée');
+      return $this->redirectToRoute('home');
     }
 
     return $this->render('question/index.html.twig', [
@@ -29,22 +40,26 @@ class QuestionController extends AbstractController
   }
 
   #[Route('/{id}', name: 'show')]
-  public function show(Request $request, string $id): Response
+  public function show(Request $request, Question $question, EntityManagerInterface $em): Response
   {
+    $comment = new Comment();
+    $commentForm = $this->createForm(CommentType::class, $comment);
+    $commentForm->handleRequest($request);
 
-    $question =   [
-      'title' => 'Je suis une super question',
-      'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, adipisci. Libero aperiam dolores excepturi, quidem maxime accusantium inventore. Illum, odio dolores! Ullam omnis veritatis laborum, animi inventore nostrum optio voluptates.',
-      'rating' => 20,
-      'author' => [
-        'name' => 'Jean Dupont',
-        'avatar' => 'https://randomuser.me/api/portraits/men/52.jpg'
-      ],
-      'nbrOfResponse' => 15
-    ];
+    if($commentForm->isSubmitted() && $commentForm->isValid()) {
+      $comment->setCreatedAt(new \DateTimeImmutable());
+      $comment->setRating(0);
+      $comment->setQuestion($question);
+      $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
+      $em->persist($comment);
+      $em->flush();
+      $this->addFlash('success', 'Votre réponse a bien été ajoutée');
+      return $this->redirect($request->getUri());
+    }
 
     return $this->render('question/show.html.twig', [
       'question' => $question,
+      'form' => $commentForm->createView()
     ]);
   }
 }
--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -2,27 +2,31 @@
 
 namespace App\Form;
 
+use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class QuestionType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('title', TextType::class)
-            ->add('content', TextareaType::class, [
-                'attr' => ['rows' => 8],
-            ]);
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->add('title', TextType::class)
+      ->add('content', TextareaType::class, [
+        'attr' => [
+          'rows' => 8,
+          'style' => 'resize: none;',
+        ],
+      ]);
+  }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            // Configure your form options here
-        ]);
-    }
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $resolver->setDefaults([
+      'data_class' => Question::class
+    ]);
+  }
 }
