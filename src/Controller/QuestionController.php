2077f3464b7d5854e4e900ae779ce646214c52ad
Yohan-Baechle <baechle.yohan.gmail.com>
2023-11-12 13:45:28 +0100

    Form ok

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\QuestionType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/question', name: 'question_')]
 class QuestionController extends AbstractController
 {
-    #[Route('/question', name: 'app_question')]
-    public function index(): Response
-    {
-        return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
-        ]);
+  #[Route('/ask', name: 'form')]
+  public function index(Request $request): Response
+  {
+
+    $formQuestion = $this->createForm(QuestionType::class);
+
+    $formQuestion->handleRequest($request);
+
+    if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
+      dd($formQuestion->getData());
     }
+
+    return $this->render('question/index.html.twig', [
+      'form' => $formQuestion->createView(),
+    ]);
+  }
+
+  #[Route('/{id}', name: 'show')]
+  public function show(Request $request, string $id): Response
+  {
+
+    $question =   [
+      'title' => 'Je suis une super question',
+      'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, adipisci. Libero aperiam dolores excepturi, quidem maxime accusantium inventore. Illum, odio dolores! Ullam omnis veritatis laborum, animi inventore nostrum optio voluptates.',
+      'rating' => 20,
+      'author' => [
+        'name' => 'Jean Dupont',
+        'avatar' => 'https://randomuser.me/api/portraits/men/52.jpg'
+      ],
+      'nbrOfResponse' => 15
+    ];
+
+    return $this->render('question/show.html.twig', [
+      'question' => $question,
+    ]);
+  }
 }
